b1356e23dbf02d9da104c166e85cf7fea08559b1
Francis KOUAHO <kouahofrancis@gmail.com>
2024-06-03 09:07:24 +0200

    feat(front): preview resolve error compilateur

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -68,7 +68,8 @@ const Page = () => {
           </div>
         </div>
       </div>
-      <div className="flex">
+
+      {/*<div className="flex">
         <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
           <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
             <Card x-chunk="dashboard-01-chunk-0">
@@ -318,7 +319,7 @@ const Page = () => {
             </Card>
           </div>
         </div>
-      </div>
+      </div>*/}
     </>
   )
 }
